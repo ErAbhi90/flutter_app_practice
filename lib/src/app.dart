d0ba905b954955ea4027efe4fb648a78e233117a
ErAbhi90 <er.abhithakral@gmail.com>
2018-08-02 15:18:10 +0530

    Converting to Stateful Widget

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,29 +1,47 @@
 import 'package:flutter/material.dart';
 
-//Custom Widget Stateless vs Stateful
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
 
-class App extends StatelessWidget {
+class AppState extends State<App> {
   // Stateless widget will not contain its own data
   //Stateful is used when we need to maintain some data
 
   Widget build(BuildContext context) {
+    //instance variable
+    int counter = 0;
+
     return MaterialApp(
 
 //Scaffold is a class used to implement basic material design features
         home: Scaffold(
-      appBar: AppBar(
-        title: Text('This is an AppBar'),
-      ),
-      floatingActionButton: FloatingActionButton(
+          appBar: AppBar(
+            title: Text('This is an AppBar'),
+          ),
 
-          // For list of ICONS visit https://material.io/tools/icons/?style=baseline
-          child: Icon(Icons.add),
+
+          floatingActionButton: FloatingActionButton(
+
+            // For list of ICONS visit https://material.io/tools/icons/?style=baseline
+              child: Icon(Icons.add),
 
 // On Pressed method is mandatory in Floating Action button
-          onPressed: () {
-            //Implementation of Button Click Here
-            print('Floating Button Clicked');
-          }),
-    ));
+              onPressed: () {
+                //Implementation of Button Click Here
+
+                setState(() {
+                  counter += 1;
+                  // print('Floating Button Clicked $counter');
+                });
+              }),
+
+          //body property to display Widget below AppBar
+          body: Text('$counter'),
+
+        )
+    );
   }
 }
