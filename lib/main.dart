beea80a455f5209dbaa2ecdd3781453e19f05342
ErAbhi90 <er.abhithakral@gmail.com>
2018-08-02 13:37:10 +0530

    Adding a Scaffold for Appbar and Floating Action Button

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 //Creating Widget
 void main(){
   var app = MaterialApp(
-      home: Text('Hi There!')
+      home: Scaffold(
+        appBar: AppBar(),
+      )
   );
 
   runApp(app);
