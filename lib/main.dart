f85b9307c574d2615d6b3dfbc8787382c788a5bd
ErAbhi90 <er.abhithakral@gmail.com>
2018-08-02 13:42:56 +0530

    Adding a Scaffold for Appbar and Floating Action Button

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,21 @@ import 'package:flutter/material.dart';
 //Creating Widget
 void main(){
   var app = MaterialApp(
+
+//Scaffold is a class used to implement basic material design features
       home: Scaffold(
-        appBar: AppBar(),
+
+        appBar: AppBar(
+          title: Text('This is an AppBar'),
+        ),
+
+// On Pressed method is mandatory in Floating Action button
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              //Implementation of Button Click Here
+
+            }),
+
       )
   );
 
