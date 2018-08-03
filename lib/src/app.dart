7c3780ef86335d8da64cfc5e0855c201904226f6
ErAbhi90 <er.abhithakral@gmail.com>
2018-08-03 12:11:05 +0530

    Fetch Image from URL on click of button and Displayed it in ListView

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' show get;
 
 import 'models/image_model.dart';
+import 'widgets/image_list.dart';
 
 class App extends StatefulWidget {
   createState() {
@@ -12,32 +13,25 @@ class App extends StatefulWidget {
 }
 
 class AppState extends State<App> {
-  // Stateless widget will not contain its own data
-  //Stateful is used when we need to maintain some data
 
-
-  //instance variable
   int counter = 0;
-
-  //List Variable
   List<ImageModel> images = [];
 
+
   //To fetch data Async
-
   void fetchImageData() async {
-    //increment counter as in our Json data starts from 1 but our counter value starts from 0
     counter++;
-
-    //To fetch data from below url and instead of loading all images at the same time we are passing counter so to fetch data one by one when button clicked
-    var response = await get(
-        'https://jsonplaceholder.typicode.com/photos/$counter');
-
+    print('Working on click  $counter');
+    var response =
+    await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    print(json.decode(response.body));
     var imageModel = ImageModel.fromJson(json.decode(response.body));
 
     setState(() {
       images.add(imageModel);
     });
   }
+
 
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -55,14 +49,11 @@ class AppState extends State<App> {
               child: Icon(Icons.add),
 
 // On Pressed method is mandatory in Floating Action button
-              onPressed: () {
-                //Implementation of Button Click Here
-
-                fetchImageData;
-              }),
+            onPressed: fetchImageData,
+          ),
 
           //body property to display Widget below AppBar
-          body: Text('This is count: $counter'),
+          body: ImageList(images),
 
         )
     );
